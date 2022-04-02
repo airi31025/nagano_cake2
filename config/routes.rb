68a527e57376d950fc4455f7955e690179b953b1
EC2 Default User <ec2-user@ip-172-31-83-218.ec2.internal>
2022-04-02 09:02:15 +0000

    [Add]トップページへの遷移

--- config/routes.rb
+++ config/routes.rb
@@ -45,6 +45,6 @@ Rails.application.routes.draw do
   sessions: 'public/sessions'
 }
 
-  root to:"homes#top"
+  root to: "public/homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
