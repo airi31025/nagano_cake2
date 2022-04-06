53eeccd6bc633b4846073291d4c3e12d561203fe
EC2 Default User <ec2-user@ip-172-31-83-218.ec2.internal>
2022-04-06 12:17:27 +0000

    [Add]会員一覧のステータスの表示

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,9 +7,9 @@ class Customer < ApplicationRecord
   has_one_attached :image
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
-  has_many :address, dependent: :destroy
-  
-  
+  has_many :addresses, dependent: :destroy
+
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
