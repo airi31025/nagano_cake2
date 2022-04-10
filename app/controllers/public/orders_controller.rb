7d77c263063323fec0fadc0c57f1e2d9c60260f1
EC2 Default User <ec2-user@ip-172-31-83-218.ec2.internal>
2022-04-10 08:20:15 +0000

    [Add]注文情報確認ページ作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,11 +1,39 @@
 class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
-   
+
   end
 
   def confirm
-    
+    @cart_items = current_customer.cart_items
+    @order = Order.new(order_params)
+    @total = 0
+    @order.shipping_cost =800
+    if params[:order][:select_address] == '0' then
+
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name+current_customer.last_name
+    elsif params[:order][:select_address] == '1' then
+
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    else
+      new_address = Address.new
+      new_address.customer_id = current_customer.id
+      new_address.name = params[:order][:name]
+      new_address.postal_code = params[:order][:postal_code]
+      new_address.address = params[:order][:address]
+      new_address.save
+      @order.name = params[:order][:name]
+      @order.postal_code = params[:order][:postal_code]
+      @order.address = params[:order][:address]
+
+
+    end
+
   end
 
   def thanks
@@ -24,4 +52,10 @@ class Public::OrdersController < ApplicationController
     @item = Item.find(params[:id])
     @cart_item = CartItem.find(params[:id])
   end
+
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+  end
+
 end
